728e3ff3e5ece096636d2d6f8c3c725caebcc434
Steven Curren <46661819+steveof2620@users.noreply.github.com>
2023-06-06 09:42:05 +1000

    Tidy
    Used MU's tidy function to clean the code a bit.

--- code.py
+++ code.py
@@ -9,7 +9,26 @@ import board
 import neopixel
 import time
 from adafruit_led_animation.animation.blink import Blink
-from adafruit_led_animation.color import RED, YELLOW, ORANGE, GREEN, TEAL, CYAN, BLUE, PURPLE, MAGENTA, WHITE, BLACK, GOLD, PINK, AQUA, JADE, AMBER, OLD_LACE
+from adafruit_led_animation.animation.colorcycle import ColorCycle
+from adafruit_led_animation.color import (
+    RED,
+    YELLOW,
+    ORANGE,
+    GREEN,
+    TEAL,
+    CYAN,
+    BLUE,
+    PURPLE,
+    MAGENTA,
+    WHITE,
+    BLACK,
+    GOLD,
+    PINK,
+    AQUA,
+    JADE,
+    AMBER,
+    OLD_LACE,
+)
 
 # on board LEDs
 # pixels = neopixel.NeoPixel(board.NEOPIXEL, 10)
@@ -18,14 +37,36 @@ from adafruit_led_animation.color import RED, YELLOW, ORANGE, GREEN, TEAL, CYAN,
 # Number of LEDs on strip is 35
 strip_pin = board.A7
 strip_num_of_lights = 35
-strip = neopixel.NeoPixel(strip_pin, strip_num_of_lights, brightness=0.5, auto_write=True)
-
-blink = Blink(strip, speed=0.5, color=JADE)
+strip = neopixel.NeoPixel(
+    strip_pin, strip_num_of_lights, brightness=0.5, auto_write=True
+)
 
 # an array of colours
-colours = [RED, YELLOW, ORANGE, GREEN, TEAL, CYAN, BLUE, PURPLE, MAGENTA, WHITE, BLACK, GOLD, PINK, AQUA, JADE, AMBER, OLD_LACE]
+colours = [
+    RED,
+    YELLOW,
+    ORANGE,
+    GREEN,
+    TEAL,
+    CYAN,
+    BLUE,
+    PURPLE,
+    MAGENTA,
+    WHITE,
+    BLACK,
+    GOLD,
+    PINK,
+    AQUA,
+    JADE,
+    AMBER,
+    OLD_LACE,
+]
+
+
+blink = Blink(strip, speed=0.5, color=OLD_LACE)
+colorcycle = ColorCycle(strip, 0.5, colors=colours)
 
 # infinite loop
 while True:
-    blink.animate()
-
+    # blink.animate()
+    colorcycle.animate()
