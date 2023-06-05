8011c40afabecab0c478fe00f6f03bd2ae4e4bca
Steven Curren <46661819+steveof2620@users.noreply.github.com>
2023-06-05 20:53:24 +1000

    LED strip found
    The LED strip is connected to pin A7 so the potentiometer is on pin A3.

--- code.py
+++ code.py
@@ -7,8 +7,14 @@
 
 import board
 import neopixel
+import time
 
-pixels = neopixel.NeoPixel(board.NEOPIXEL, 10)
+# pixels = neopixel.NeoPixel(board.NEOPIXEL, 10)
+
+# LED strip is connected to pin A7 so the potentiometer is on pin A3
+strip_pin = board.A7
+strip_num_of_lights = 5
+strip = neopixel.NeoPixel(strip_pin, strip_num_of_lights, brightness = 0.5, auto_write=True)
 
 # colour codes RGB
 RED = (255, 0, 0)
@@ -19,8 +25,13 @@ BLUE = (0, 0, 255)
 PURPLE = (180, 0, 255)
 OFF = (0, 0, 0)
 
-pixels.fill((OFF))
+# an array of colours
+colours = [RED, YELLOW, GREEN, CYAN, BLUE, PURPLE]
 
 # infinite loop
 while True:
-    pass
+    for i in range(len(strip)):
+        strip[i] = colours[i]
+        time.sleep(0.3)
+    strip.fill(OFF)
+
