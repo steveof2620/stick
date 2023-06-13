e6d68b63abad7285a1fe54b78822e9f69f9afdd6
Steven Curren <46661819+steveof2620@users.noreply.github.com>
2023-06-14 09:58:25 +1000

    Update code.py

--- code.py
+++ code.py
@@ -1,37 +1,31 @@
-# First test for the on board LEDs
+# Write your code here :-)
+# SPDX-FileCopyrightText: 2018 Kattni Rembor for Adafruit Industries
+#
+# SPDX-License-Identifier: MIT
 
-# Conventions
-# constants, ALL_CAPS
-# Class names, UpperCamelCase
-# Vairable, lower case
-
+import time
 import board
 import neopixel
-import time
+from analogio import AnalogIn
+from adafruit_simplemath import map_range
 
-# pixels = neopixel.NeoPixel(board.NEOPIXEL, 10)
+NUMPIXELS = 10  # Circuit Playground Express has 10 pixels
+pixels = neopixel.NeoPixel(board.NEOPIXEL, NUMPIXELS, auto_write=False)  # CPX NeoPixels
+potentiometer = AnalogIn(board.A1)  # potentiometer connected to A1, power & ground
 
-# LED strip is connected to pin A7 so the potentiometer is on pin A3
-strip_pin = board.A7
-strip_num_of_lights = 5
-strip = neopixel.NeoPixel(strip_pin, strip_num_of_lights, brightness = 0.5, auto_write=True)
+def show_value(val):            # Show value 0-9 on CPX NeoPixels
+    for i in range(val):
+        pixels[i] = (50, 0, 0)  # Red
+    for i in range(val, NUMPIXELS):
+        pixels[i] = (0, 0, 0)
+    pixels.show()
 
-# colour codes RGB
-RED = (255, 0, 0)
-YELLOW = (255, 150, 0)
-GREEN = (0, 255, 0)
-CYAN = (0, 255, 255)
-BLUE = (0, 0, 255)
-PURPLE = (180, 0, 255)
-OFF = (0, 0, 0)
-
-# an array of colours
-colours = [RED, YELLOW, GREEN, CYAN, BLUE, PURPLE]
-
-# infinite loop
 while True:
-    for i in range(len(strip)):
-        strip[i] = colours[i]
-        time.sleep(0.3)
-    strip.fill(OFF)
+    remapped_value = int(map_range(potentiometer.value, 0, 65520, 0, 100))
+    show_value(int(potentiometer.value / 65520 * NUMPIXELS))  # Show on NeoPixels
+    print((potentiometer.value,))# Print value
+    print ((remapped_value,))
+
+    time.sleep(0.25)                   # Wait a bit before checking all again
+
 
